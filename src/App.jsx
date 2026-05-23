6823195cce006ee4f14397778095cab85e8b5bad
Maxdeoro <maxdeoro@gmail.com>
2026-05-24 00:47:02 +0300

    add open/close sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CompletedTaskList } from './completedTaskList/CompletedTaskList';
 import { Footer } from './footer/Footer';
 import './index.css';
@@ -5,28 +6,50 @@ import { TaskForm } from './taskForm/TaskForm';
 import { TaskList } from './taskList/TaskList';
 
 function App() {
-  return <div className="app">
-    <div className="task-container">
-      <h1>Task List with Priority</h1>
-      <button className="close-button">+</button>
-      <TaskForm />
-    </div>
-    <div className="task-container">
-      <h2>Tasks</h2>
-      <button className="close-button">+</button>
-      <div className='sort-controls'>
-        <button className='sort-button'>By Date</button>
-        <button className='sort-button'>By Priority</button>
+
+  const [openSection,setOpenSection] = useState({
+    taskList: false,
+    tasks: true,
+    completedTasks: true,
+  });
+
+  function toggleSection(section) {
+    setOpenSection((prevValue) => ({
+      ...prevValue,
+      [section]: !prevValue[section],
+    }));
+  };
+
+  return (
+    <div className="app">
+      <div className="task-container">
+        <h1>Task List with Priority</h1>
+        <button className={`close-button ${openSection.taskList ? "open" : ""}`}
+                onClick={() => toggleSection('taskList')}
+        >+</button>
+        {openSection.taskList && <TaskForm />}
       </div>
-      <TaskList />
+      <div className="task-container">
+        <h2>Tasks</h2>
+        <button className={`close-button ${openSection.tasks ? "open" : ""}`}
+                onClick={() => toggleSection('tasks')}
+        >+</button>
+        <div className='sort-controls'>
+          <button className='sort-button'>By Date</button>
+          <button className='sort-button'>By Priority</button>
+        </div>
+        {openSection.tasks && <TaskList />}
+      </div>
+      <div className="completed-task-container">
+        <h2>Completed Tasks</h2>
+        <button className={`close-button ${openSection.completedTasks ? "open" : ""}`}
+                onClick={() => toggleSection('completedTasks')}
+        >+</button>
+        {openSection.completedTasks && <CompletedTaskList />}
+      </div>
+      <Footer />
     </div>
-    <div className="completed-task-container">
-      <h2>Completed Tasks</h2>
-      <button className="close-button">+</button>
-      <CompletedTaskList />
-    </div>
-    <Footer />
-  </div>;
+  )
 }
 
 export default App;
